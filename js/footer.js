3e70fa6fa7f6f23d00737ac96c86d69d7521ddf0
anupama cv <anu.anupamacv@gmail.com>
2021-08-06 21:30:23 +0530

    Added: functionality to create about dlc page and press page

---
+++ js/footer.js
@@ -0,0 +1,45 @@
+document.getElementById("footer").innerHTML =
+`<footer class="footer">
+    <div>
+        <div class="left">
+            <div class="column">
+                <h1 style="list-style-type:none">
+                    <li><a href="index.html"><img src="../assets/logo.jpg" alt=""></a></li> 
+                </h1>
+                <div class="coloumn-bottom-part">
+                    <h2 style="list-style-type:none">
+                        <li><a>@ Discovery Land Company. All rights reserved.</a></li>
+                    </h2>
+                </div> 
+            </div>
+            <div class="right">
+                <div class="column1">
+                    <h1 style="list-style-type:none">
+                        <li><a href="">GET IN TOUCH</a></li>
+                        <li><a href="">(480)624-5200</a></li>
+                        <li><a1 href="">mail@discoveryIndigo.com</a1></li>
+                    <h1> 
+                </div>
+                <div class="column2">
+                    <h1 style="list-style-type:none">
+                        <li><a href="">About DLC</a></li>
+                        <li><a href="">Our Worlds</a></li>
+                        <li><a href="">Experiances</a></li>
+                        <li><a href="">Gallary</a></li>
+                        <li><a href="">Press</a></li>
+                    <h1> 
+                </div>
+                <div class="column3">
+                    <h1 style="list-style-type:none">
+                        <li><a href="">Magazine</a></li>
+                        <li><a href="">Careers</a></li>
+                        <li><a href="">Foundation</a></li>
+                        <li><a href="">Company</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li><a href="">Privacy</a></li>
+                    <h1> 
+                </div> 
+            </div>
+        </div>
+    </div> 
+</footer>`
---
+++ js/header.js
@@ -0,0 +1,18 @@
+document.getElementById("header").innerHTML = `<div class="header">
+<nav>
+    <a href="index.html"><img src="../assets/logo.jpg" alt=""></a>
+    <div class="name">
+        <h1>Discovery</h1>
+        <h2>LAND COMPANY</h2>
+    </div>
+    <div class="options">
+        <h3>
+            <span><a href="">About DLC</a></span>
+            <span><a href="http://127.0.0.1:5500/pages/our_world.html">Our Worlds</a></span>
+            <span><a href="">Experiances</a></span>
+            <span><a href="">Gallary</a></span>
+            <span><a href="http://127.0.0.1:5500/pages/press.html">Press</a></span>
+            <i class="fa fa-bars"></i>
+        </h3>
+    </div>
+</nav> `
