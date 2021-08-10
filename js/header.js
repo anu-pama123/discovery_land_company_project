f33d098fbf45ab39eb2ef3f14c2777f5a76ff2a7
anupama cv <anu.anupamacv@gmail.com>
2021-08-10 11:44:27 +0530

    Added: functionality to create company and carrers inner pages of discovery land project

--- js/header.js
+++ js/header.js
@@ -1,18 +1,19 @@
-document.getElementById("header").innerHTML = `<div class="header">
+document.getElementById("header-content").innerHTML = `<header class="header-content">
 <nav>
-    <a href="index.html"><img src="../assets/logo.jpg" alt=""></a>
+    <a href="index.html"><img src="../assets/logo1.png" alt=""></a> 
     <div class="name">
-        <h1>Discovery</h1>
-        <h2>LAND COMPANY</h2>
     </div>
     <div class="options">
         <h3>
-            <span><a href="">About DLC</a></span>
-            <span><a href="http://127.0.0.1:5500/pages/our_world.html">Our Worlds</a></span>
-            <span><a href="">Experiances</a></span>
-            <span><a href="">Gallary</a></span>
-            <span><a href="http://127.0.0.1:5500/pages/press.html">Press</a></span>
+            <span><a href="http://127.0.0.1:5501/pages/aboutdlc.html">About DLC</a></span>
+            <span><a href="http://127.0.0.1:5501/pages/our_world.html">Our Worlds</a></span>
+            <span><a href="http://127.0.0.1:5501/pages/experiance.html">Experiances</a></span>
+            <span><a href="http://127.0.0.1:5501/pages/gallary.html">Gallary</a></span>
+            <span><a href="http://127.0.0.1:5501/pages/press.html">Press</a></span>
             <i class="fa fa-bars"></i>
         </h3>
     </div>
-</nav> `
+</nav>
+</header> `
+
+
